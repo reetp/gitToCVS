079063fabb0aeabf10fdf8a91996336c6d6820a4
John Crisp <jcrisp@safeandsoundit.co.uk>
2016-03-10 17:16:26 +0100

    More fixes. Basically works

--- gitToCVS.sh
+++ gitToCVS.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Clean out your contrib directory
+# cvsupdate -dPA
+# make clean
+# make prep
+
+# git pull your changes
+# make sure in your spec file you have the new version number and Patch file name
+# The script will look for something like
+# Patch1: MynewPatch.patch
+# And will create it
+
 #Set Script Name variable
 SCRIPT=`basename ${BASH_SOURCE[0]}`
 
@@ -14,27 +25,27 @@ GITDIR=""
 CVSDIR=""
 HOMEDIR=""
 
-while getopts ":c:v:s:h" opt; do
+while getopts ":c:v:s:d:h" opt; do
   case $opt in
     c)
-      echo "-c was triggered, Parameter: $OPTIND - $OPTARG" >&2
+#      echo "-c was triggered, Parameter: $OPTIND - $OPTARG" >&2
       CONTRIB=$OPTARG
-      echo "$CONTRIB - $OPTARG"
+#      echo "$CONTRIB - $OPTARG"
       ;;
     v)
-      echo "-v was triggered, Parameter: $OPTIND - $OPTARG" >&2
+#      echo "-v was triggered, Parameter: $OPTIND - $OPTARG" >&2
       VERSION=$OPTARG
-      echo "$VERSION - $OPTARG"
+#      echo "$VERSION - $OPTARG"
       ;;
     s)
-      echo "-s was triggered, Parameter: $OPTIND - $OPTARG" >&2
+#      echo "-s was triggered, Parameter: $OPTIND - $OPTARG" >&2
       SMEVERSION=$OPTARG
-      echo "$SMEVER - $OPTARG"
+#      echo "$SMEVER - $OPTARG"
       ;;
     d)
-      echo "-s was triggered, Parameter: $OPTIND - $OPTARG" >&2
+#      echo "-s was triggered, Parameter: $OPTIND - $OPTARG" >&2
       HOMEDIR=$OPTARG
-      echo "$HOMEDIR - $OPTARG"
+#      echo "$HOMEDIR - $OPTARG"
       ;;
     h)  #show help
       echo -e "Example: $SCRIPT -c smeserver-contrib -v 0.5 -s 9"\\n
@@ -52,22 +63,19 @@ while getopts ":c:v:s:h" opt; do
   esac
 done
 
+# Note to self on variable numbers
 # -c = $4
 # -v = $6
 # -s = $8
 # -d =$10
 
-# cd ~/git/smeserver-libreswan
-
-#
-
+# Set the git directory
 GITDIR="$HOMEDIR/git/$CONTRIB"
-echo "gitdir = $GITDIR"
+#echo "gitdir = $GITDIR"
 
+# Set the cvs directory
 CVSDIR="$HOMEDIR/smecontribs/rpms/smeserver-libreswan/contribs$SMEVERSION"
-echo "cvsdir = $CVSDIR"
-
-#echo "cd ~/smecontribs/rpms/smeserver-libreswan/contribs$SMEVERSION"
+#echo "cvsdir = $CVSDIR"
 
 # Move the old CVS to .old
 mv $CVSDIR/$CONTRIB-$VERSION $CVSDIR/$CONTRIB-$VERSION.old
@@ -82,10 +90,13 @@ diff -ruN $CVSDIR/smeserver-libreswan.spec $GITDIR/smeserver-libreswan.spec |gre
 DIFF=$(diff -ruN $CVSDIR/smeserver-libreswan.spec $GITDIR/smeserver-libreswan.spec | grep Patch | sed 's/.*: //')
 
 # Now diff the old and new directories and create the patch file
-diff -ruN $CVSDIR/$CONTRIB-$VERSION.old $CVSDIR/$CONTRIB-$VERSION > /$CVSDIR/$DIFF
+cd $CVSDIR
+
+diff -ruN ./$CONTRIB-$VERSION.old ./$CONTRIB-$VERSION > /$CVSDIR/$DIFF
 
 echo "DIFF is $DIFF"
 
 # Now copy the spec file
 /bin/cp -rf $GITDIR/smeserver-libreswan.spec $CVSDIR/smeserver-libreswan.spec
 
+echo "Before you run me again clean your CVS directory"
